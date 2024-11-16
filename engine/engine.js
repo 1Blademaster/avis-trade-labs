c3277f0b895cd8662471ce68d62dbe0edf7c6336
Dan Pickford <danpickford.uk@gmail.com>
2024-11-16 17:20:32 +0000

    Ensured that only a single instance of the server is running at a time.

--- engine/engine.js
+++ engine/engine.js
@@ -1,13 +1,15 @@
 const fs = require('fs')
 const path = require('path')
 const csv = require('fast-csv')
+const SingleInstance = require('single-instance');
 
+const locker = new SingleInstance('my-app-name');
 const data = []
 
 const fileNames = [
-  'data/btcusd_2016-min_data.csv',
-  'data/btcusd_2018-min_data.csv',
-  'data/btcusd_2020-min_data.csv',
+  'data/btcusd_2016-min_data.csv', 
+  'data/btcusd_2018-min_data.csv', 
+  'data/btcusd_2020-min_data.csv', 
   'data/btcusd_2022-min_data.csv',
 ]
 
@@ -24,8 +26,6 @@ function loopThroughFiles(currentFileIndex) {
     })
 }
 
-loopThroughFiles(0)
-
 var current = 0
 export var btcData = {
   open: 0,
@@ -34,10 +34,17 @@ export var btcData = {
 }
 export var btcClose = 0
 
-setInterval(() => {
-  current = current + 1
-  btcData.open = parseFloat(data[current]?.Open)
-  btcData.close = parseFloat(data[current]?.Close)
-  btcData.time = parseInt(data[current]?.Timestamp)
-  console.log(current)
-}, 100)
+locker.lock().then(() => {
+  loopThroughFiles(0)
+
+  setInterval(() => {
+    current = current + 1
+    btcData.open = parseFloat(data[current]?.Open)
+    btcData.close = parseFloat(data[current]?.Close)
+    btcData.time = parseInt(data[current]?.Timestamp)
+    console.log(current)
+  }, 100)
+}).catch(err => {
+  // This block will be executed if the app is already running
+  console.log(err); // it will print out 'An application is already running'
+});
