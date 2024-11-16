94be7d605ff5265594d3f00ce8de5a400fad14e9
Kush Makkapati <kush.makkapati@icloud.com>
2024-11-16 14:59:18 +0000

    Add realtime graph

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { ColorSchemeScript } from '@mantine/core'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
+    <Html lang='en' suppressHydrationWarning>
+      <Head>
+        <ColorSchemeScript defaultColorScheme='auto' />
+      </Head>
+      <body>
+        <Main className='h-full' />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
