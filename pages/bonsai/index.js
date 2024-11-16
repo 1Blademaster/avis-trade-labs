7d7a5239757816674b17434ac0a2e1c17c3ff8e2
Julian <julianjones663@gmail.com>
2024-11-16 22:55:27 +0000

    added line crossing detection - cant automate now!

--- pages/bonsai/index.js
+++ pages/bonsai/index.js
@@ -9,7 +9,7 @@ export default function Bonsai() {
   const height = 1080;
   const centerX = width / 2;
   const centerY = height / 2;
-  const lineLength = 35;
+  const lineLength = 50;
   const gap = 10;
   const startingPoint = [centerX, height];
 
@@ -28,10 +28,17 @@ export default function Bonsai() {
     [up, "2r2", false, "3r3"],
     [right, "2r3", false, "3r4"],
     // 4th row
-    [right, "3r1", false, "4r1"],
-    [left, "3r2", false, "4r2"],
+    [right, "3r1", false, "4r1", true],
+    [left, "3r2", false, "4r2", true],
     [right, "3r2", false, "4r3"],
-    [right, "3r3", false, "4r4"],
+    [right, "3r3", false, "4r4", true],
+    [left, "3r4", false, "4r5", true],
+    [right, "3r4", false, "4r6"],
+    [left, "3r3", false, "4r7"], // Testing
+    // 5th row
+    [up, "4r2", false, "5r1"],
+    [left, "4r3", false, "5r2"],
+    [right, "4r3", false, "5r3"],
   ];
 
   let references = {};
@@ -41,35 +48,85 @@ export default function Bonsai() {
     const ctx = canvas.getContext("2d");
 
     points.forEach((point) => {
-      console.log(references);
+      // Get variables from point
       let passedAngle = point[0];
       let referencePoint = point[1];
       let highlighted = point[2];
       let label = point[3];
+      let touching = point[4] ? point[4] : false;
 
-      let angle = passedAngle + 90;
-      let xDiff = lineLength / Math.tan((angle * Math.PI) / 180);
-      let yDiff = lineLength;
-
+      // Get what line we're starting from
       let xRef;
       let yRef;
-      if (referencePoint == -1) {
-        xRef = startingPoint[0];
-        yRef = startingPoint[1];
-      } else {
-        xRef = references[referencePoint][0];
-        yRef = references[referencePoint][1];
+      let shouldBeBigger;
+      try {
+        if (referencePoint == -1) {
+          xRef = startingPoint[0];
+          yRef = startingPoint[1];
+          shouldBeBigger = false;
+        } else {
+          xRef = references[referencePoint][1];
+          yRef = references[referencePoint][3];
+          shouldBeBigger = references[referencePoint][4];
+        }
+      } catch {
+        console.log(
+          "Failed on line: " + label + "trying to reference " + referencePoint
+        );
+        console.log(references);
       }
 
+      // Find length and difference in x and y
+      let lineLengthVaried = touching ? lineLength - gap : lineLength;
+      lineLengthVaried = shouldBeBigger
+        ? lineLengthVaried + gap
+        : lineLengthVaried;
+      let angle = passedAngle + 90;
+      let xDiff = lineLengthVaried / Math.tan((angle * Math.PI) / 180);
+      let yDiff = lineLengthVaried;
+
+      // Find starting and endpoints
       let xStart =
         xRef - (passedAngle < 0 ? gap : 0) + (passedAngle > 0 ? gap : 0);
       let yStart = yRef - gap;
-
       let xEnd = xStart - xDiff;
       let yEnd = yStart - yDiff - gap;
 
-      references[label] = [xEnd, yEnd];
+      // [xStart, xEnd, yStart, yEnd, touching]
+      references[label] = [
+        xStart,
+        touching ? xEnd - gap : xEnd,
+        yStart,
+        yEnd,
+        touching,
+      ];
 
+      // Check for intersections
+      for (let i = 0; i < Object.keys(references).length; i++) {
+        let secondLine = references[Object.keys(references)[i]];
+        if (
+          willCross(
+            xStart,
+            yStart,
+            xEnd,
+            yEnd,
+            secondLine[0],
+            secondLine[2],
+            secondLine[1],
+            secondLine[3]
+          ) &&
+          label != Object.keys(references)[i]
+        ) {
+          console.log(
+            "THIS LINE WILL CROSS " +
+              label +
+              " WITH " +
+              Object.keys(references)[i]
+          );
+        }
+      }
+
+      // Draw line
       ctx.beginPath();
       ctx.moveTo(xStart, yStart);
       ctx.lineTo(xEnd, yEnd);
@@ -90,4 +147,30 @@ export default function Bonsai() {
       height={height}
     ></canvas>
   );
+}
+
+function willCross(p0x, p0y, p1x, p1y, p2x, p2y, p3x, p3y) {
+  let d1x = p1x - p0x;
+  let d1y = p1y - p0y;
+  let d2x = p3x - p2x;
+  let d2y = p3y - p2y;
+  let d = d1x * d2y - d2x * d1y;
+  let px;
+  let py;
+  let s;
+  let t;
+
+  if (d) {
+    px = p0x - p2x;
+    py = p0y - p2y;
+
+    s = (d1x * py - d1y * px) / d;
+    if (s >= 0 && s <= 1) {
+      t = (d2x * py - d2y * px) / d;
+      if (t >= 0 && t <= 1) {
+        return { x: p0x + t * d1x, y: p0y + t * d1y };
+      }
+    }
+  }
+  return null;
 }
