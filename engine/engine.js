2fd9efa2678ecf2de606e280e0772de657f9dbbb
Kush Makkapati <kush.makkapati@icloud.com>
2024-11-16 15:57:51 +0000

    Add integration of data to realtime graph

--- engine/engine.js
+++ engine/engine.js
@@ -5,13 +5,13 @@ const csv = require('fast-csv')
 const data = []
 
 const fileNames = [
-  'data/btcusd_2016-min_data.csv', 
-  'data/btcusd_2018-min_data.csv', 
-  'data/btcusd_2020-min_data.csv', 
-  'data/btcusd_2022-min_data.csv'
+  'data/btcusd_2016-min_data.csv',
+  'data/btcusd_2018-min_data.csv',
+  'data/btcusd_2020-min_data.csv',
+  'data/btcusd_2022-min_data.csv',
 ]
 
-function loopThroughFiles(currentFileIndex){
+function loopThroughFiles(currentFileIndex) {
   fs.createReadStream(fileNames[currentFileIndex])
     .pipe(csv.parse({ headers: true }))
     .on('error', (error) => console.error(error))
@@ -20,17 +20,24 @@ function loopThroughFiles(currentFileIndex){
     })
     .on('end', (rowCount) => {
       console.log(`Parsed ${rowCount} rows`)
-      loopThroughFiles((currentFileIndex+1)%fileNames.length)
+      loopThroughFiles((currentFileIndex + 1) % fileNames.length)
     })
 }
 
 loopThroughFiles(0)
 
 var current = 0
+export var btcData = {
+  open: 0,
+  close: 0,
+  time: null,
+}
 export var btcClose = 0
 
 setInterval(() => {
   current = current + 1
-  btcClose = data[current]?.Close
-  console.log(data.length, current, data[current], btcClose)
-}, 1000)
+  btcData.open = parseFloat(data[current]?.Open)
+  btcData.close = parseFloat(data[current]?.Close)
+  btcData.time = parseInt(data[current]?.Timestamp)
+  console.log(current)
+}, 100)
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
 import RealtimeGraph from '@/components/realtimeGraph'
 import { useInterval } from '@mantine/hooks'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
 
 export default function Home() {
   const ref = useRef(null)
-  const [t, setT] = useState(0)
 
   const interval = useInterval(() => {
     async function fetchPosts() {
       let res = await fetch('/api/hello')
-      let returnedData = await res.json()
-      const data = { x: Date.now(), y: t }
+      let btcData = await res.json()
+      if (btcData.time === null) return
+
+      const data = { x: Date.now(), y: btcData.close }
       ref?.current.data.datasets[0].data.push(data)
       console.log(data)
       ref?.current.update('quiet')
-      setT((prev) => prev + 1)
     }
     fetchPosts()
-  }, 1000)
+  }, 100)
 
   useEffect(() => {
     interval.start()
