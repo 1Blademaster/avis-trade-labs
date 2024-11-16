816dc8d2f3097dcd0772530354c196a59219b023
Joe Paton <joantpat@gmail.com>
2024-11-16 23:56:29 +0000

    Added user to navbar

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,12 @@
+import { useUser } from '@auth0/nextjs-auth0/client'
+import { Button, Group, Menu, UnstyledButton } from '@mantine/core';
+import { IconChevronDown } from '@tabler/icons-react';
 import Link from 'next/link'
 
 export default function Navbar() {
+
+  const {user, isLoading, err} = useUser();
+
   return (
     <div className='flex flex-row justify-center items-center space-x-8 py-4 bg-slate-800'>
       <Link href='/'>
@@ -11,6 +17,21 @@ export default function Navbar() {
       <Link href='/'>Play</Link>
       <Link href='/practice'>Practice</Link>
       <Link href='/about'>About</Link>
+      <Menu>
+        <Menu.Target>
+          <UnstyledButton>
+            <Group justify='space-around'>
+              { isLoading ? "Loading" : (user ? user.email : "Log In")}
+              <IconChevronDown size="1rem"/>
+            </Group>
+          </UnstyledButton>
+        </Menu.Target>
+        <Menu.Dropdown>
+          <Menu.Item component={Link} href="/api/auth/logout">
+            Sign Out
+          </Menu.Item>
+        </Menu.Dropdown>    
+      </Menu>
     </div>
   )
 }
