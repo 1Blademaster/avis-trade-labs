a0c8b6014c762905bf4eab03fd03e5555c065773
Joe Paton <joantpat@gmail.com>
2024-11-16 16:26:59 +0000

    Added modal before logging in

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import RealtimeGraph from '@/components/realtimeGraph'
-import { useInterval } from '@mantine/hooks'
+import { Modal, Overlay } from '@mantine/core'
+import { useDisclosure, useInterval } from '@mantine/hooks'
 import { useEffect, useRef, useState } from 'react'
+import { useUser } from '@auth0/nextjs-auth0/client'
 
 export default function Home() {
   const ref = useRef(null)
   const [t, setT] = useState(0)
+
+  const {user, error, isLoading } = useUser();
+  const [opened, {open, close}] = useDisclosure();
 
   const interval = useInterval(() => {
     async function fetchPosts() {
@@ -12,7 +17,7 @@ export default function Home() {
       let returnedData = await res.json()
       const data = { x: Date.now(), y: t }
       ref?.current.data.datasets[0].data.push(data)
-      console.log(data)
+      //console.log(data)
       ref?.current.update('quiet')
       setT((prev) => prev + 1)
     }
@@ -27,6 +32,20 @@ export default function Home() {
   return (
     <div className='h-full'>
       <RealtimeGraph ref={ref} datasetLabel={'BTC'} lineColor={'#ff0000'} />
+      <Modal 
+        opened={!user} 
+        title="Authentication" 
+        overlayProps={{
+            backgroundOpacity: 0.3,
+            blur: 3
+        }}
+        bg={"blue"}
+        centered
+        onClose={user ? close : () => {}}
+        withCloseButton={false}
+      >
+        {/* Modal content */}
+      </Modal>
     </div>
   )
 }
