ec19a16e749608efb8a1e1ea595a3af37ce38b4d
Julian <julianjones663@gmail.com>
2024-11-16 23:58:08 +0000

    it generates

--- pages/bonsai/index.js
+++ pages/bonsai/index.js
@@ -8,39 +8,177 @@ export default function Bonsai() {
   const width = 1920;
   const height = 1080;
   const centerX = width / 2;
-  const centerY = height / 2;
-  const lineLength = 50;
+  const lineLength = 35;
   const gap = 10;
   const startingPoint = [centerX, height];
 
-  let points = [
-    // Starting row
-    [up, -1, true, "s"],
-    [left, "s", false, "s1"],
-    [right, "s", false, "s2"],
-    // 2nd row
-    [left, "s1", false, "2r1"],
-    [right, "s1", false, "2r2"],
-    [up, "s2", false, "2r3"],
-    // 3rd row
-    [left, "2r1", false, "3r1"],
-    [right, "2r1", false, "3r2"],
-    [up, "2r2", false, "3r3"],
-    [right, "2r3", false, "3r4"],
-    // 4th row
-    [right, "3r1", false, "4r1", true],
-    [left, "3r2", false, "4r2", true],
-    [right, "3r2", false, "4r3"],
-    [right, "3r3", false, "4r4", true],
-    [left, "3r4", false, "4r5", true],
-    [right, "3r4", false, "4r6"],
-    // 5th row
-    [up, "4r2", false, "5r1"],
-    [left, "4r3", false, "5r2"],
-    [right, "4r3", false, "5r3"],
-  ];
+  // let points = [
+  //   // Starting row
+  //   [up, -1, true, "s"],
+  //   [left, "s", false, "s1"],
+  //   [right, "s", false, "s2"],
+  //   // 2nd row
+  //   [left, "s1", false, "2r1"],
+  //   [right, "s1", false, "2r2"],
+  //   [up, "s2", false, "2r3"],
+  //   // 3rd row
+  //   [left, "2r1", false, "3r1"],
+  //   [right, "2r1", false, "3r2"],
+  //   [up, "2r2", false, "3r3"],
+  //   [right, "2r3", false, "3r4"],
+  //   // 4th row
+  //   [right, "3r1", false, "4r1", true],
+  //   [left, "3r2", false, "4r2", true],
+  //   [right, "3r2", false, "4r3"],
+  //   [right, "3r3", false, "4r4", true],
+  //   [left, "3r4", false, "4r5", true],
+  //   [right, "3r4", false, "4r6"],
+  //   // 5th row
+  //   [up, "4r2", false, "5r1"],
+  //   [left, "4r3", false, "5r2"],
+  //   [right, "4r3", false, "5r3"],
+  // ];
 
-  let references = {};
+  // let references = {};
+
+  let points = [
+    // direction, referenceLabel, isGLowing, label
+    // Starting row
+    [up, -1, true, "0r0"],
+    [left, "0r0", false, "1r0"],
+    [right, "0r0", false, "1r1"],
+  ];
+  let references = {
+    // label: [xStart, xEnd, yStart, yEnd, touching]
+    "0r0": [960, 960, 1070, 1010, false],
+    "1r0": [950, 900, 1000, 940, false],
+    "1r1": [970, 1020, 1000, 940, false],
+  };
+
+  function generateLines(ctx, maxHeight) {
+    let validDirections = [up, left, right];
+    let row = 2;
+
+    // Look at each branch that's on the row below, pick a valid branch and place it at random.
+    // Tick up row and continue till max
+    for (let currentRow = row; currentRow < maxHeight; currentRow++) {
+      let previousRow = currentRow - 1;
+      let previousKeys = Object.keys(references).filter((x) =>
+        x.startsWith(`${previousRow}r`)
+      );
+
+      console.log(references);
+
+      console.log("Starting with previous row of " + previousRow);
+      let branchId = 0;
+      previousKeys.forEach((key) => {
+        if (Math.random() >= 0.1) {
+          validDirections.forEach((direction) => {
+            let [canDraw, xStart, xEnd, yStart, yEnd] = testPath(
+              direction,
+              key,
+              `${currentRow}r${branchId}`,
+              false
+            );
+
+            if (canDraw && Math.random() >= 0.3) {
+              console.log(
+                "Drawing branch of label " +
+                  `${currentRow}r${branchId} from ${key}`
+              );
+              references[`${currentRow}r${branchId}`] = [
+                xStart,
+                xEnd,
+                yStart,
+                yEnd,
+                false,
+              ];
+              branchId++;
+
+              // Draw line
+              ctx.beginPath();
+              ctx.moveTo(xStart, yStart);
+              ctx.lineTo(xEnd, yEnd);
+              ctx.lineWidth = 4;
+              ctx.shadowBlur = false ? 10 : 0;
+              ctx.lineCap = "round";
+              ctx.shadowColor = "blue";
+              ctx.strokeStyle = false ? "white" : "gray";
+              ctx.stroke();
+            }
+          });
+        }
+      });
+    }
+  }
+
+  function testPath(passedAngle, referencePoint, label, touching) {
+    // Get what line we're starting from
+    let xRef;
+    let yRef;
+    let shouldBeBigger;
+    try {
+      if (referencePoint == -1) {
+        xRef = startingPoint[0];
+        yRef = startingPoint[1];
+        shouldBeBigger = false;
+      } else {
+        xRef = references[referencePoint][1];
+        yRef = references[referencePoint][3];
+        shouldBeBigger = references[referencePoint][4];
+      }
+    } catch (exception) {
+      console.log(
+        "Failed on line: " + label + " trying to reference " + referencePoint
+      );
+      console.log(references);
+      console.log(exception);
+    }
+
+    // Find length and difference in x and y
+    let lineLengthVaried = touching ? lineLength - gap : lineLength;
+    lineLengthVaried = shouldBeBigger
+      ? lineLengthVaried + gap
+      : lineLengthVaried;
+    let angle = passedAngle + 90;
+    let xDiff = lineLengthVaried / Math.tan((angle * Math.PI) / 180);
+    let yDiff = lineLengthVaried;
+
+    // Find starting and endpoints
+    let xStart =
+      xRef - (passedAngle < 0 ? gap : 0) + (passedAngle > 0 ? gap : 0);
+    let yStart = yRef - gap;
+    let xEnd = xStart - xDiff;
+    let yEnd = yStart - yDiff - gap;
+
+    // Check for intersections
+    for (let i = 0; i < Object.keys(references).length; i++) {
+      let secondLine = references[Object.keys(references)[i]];
+      if (
+        willCross(
+          xStart,
+          yStart,
+          xEnd,
+          yEnd,
+          secondLine[0],
+          secondLine[2],
+          secondLine[1],
+          secondLine[3]
+        ) &&
+        label != Object.keys(references)[i]
+      ) {
+        console.log(
+          "Skipping line " +
+            label +
+            " as it will cross with " +
+            Object.keys(references)[i]
+        );
+        return [false, null, null, null, null];
+      }
+    }
+
+    return [true, xStart, xEnd, yStart, yEnd];
+  }
 
   useEffect(() => {
     const canvas = document.getElementById("bonsai-canvas");
@@ -54,88 +192,38 @@ export default function Bonsai() {
       let label = point[3];
       let touching = point[4] ? point[4] : false;
 
-      // Get what line we're starting from
-      let xRef;
-      let yRef;
-      let shouldBeBigger;
-      try {
-        if (referencePoint == -1) {
-          xRef = startingPoint[0];
-          yRef = startingPoint[1];
-          shouldBeBigger = false;
-        } else {
-          xRef = references[referencePoint][1];
-          yRef = references[referencePoint][3];
-          shouldBeBigger = references[referencePoint][4];
-        }
-      } catch {
-        console.log(
-          "Failed on line: " + label + "trying to reference " + referencePoint
-        );
-        console.log(references);
+      // Test to see if we can draw the line
+      let [canDraw, xStart, xEnd, yStart, yEnd] = testPath(
+        passedAngle,
+        referencePoint,
+        label,
+        touching
+      );
+
+      if (canDraw) {
+        // [xStart, xEnd, yStart, yEnd, touching]
+        references[label] = [
+          xStart,
+          touching ? xEnd - gap : xEnd,
+          yStart,
+          yEnd,
+          touching,
+        ];
+
+        // Draw line
+        ctx.beginPath();
+        ctx.moveTo(xStart, yStart);
+        ctx.lineTo(xEnd, yEnd);
+        ctx.lineWidth = 4;
+        ctx.shadowBlur = highlighted ? 10 : 0;
+        ctx.lineCap = "round";
+        ctx.shadowColor = "blue";
+        ctx.strokeStyle = highlighted ? "white" : "gray";
+        ctx.stroke();
       }
-
-      // Find length and difference in x and y
-      let lineLengthVaried = touching ? lineLength - gap : lineLength;
-      lineLengthVaried = shouldBeBigger
-        ? lineLengthVaried + gap
-        : lineLengthVaried;
-      let angle = passedAngle + 90;
-      let xDiff = lineLengthVaried / Math.tan((angle * Math.PI) / 180);
-      let yDiff = lineLengthVaried;
-
-      // Find starting and endpoints
-      let xStart =
-        xRef - (passedAngle < 0 ? gap : 0) + (passedAngle > 0 ? gap : 0);
-      let yStart = yRef - gap;
-      let xEnd = xStart - xDiff;
-      let yEnd = yStart - yDiff - gap;
-
-      // [xStart, xEnd, yStart, yEnd, touching]
-      references[label] = [
-        xStart,
-        touching ? xEnd - gap : xEnd,
-        yStart,
-        yEnd,
-        touching,
-      ];
-
-      // Check for intersections
-      for (let i = 0; i < Object.keys(references).length; i++) {
-        let secondLine = references[Object.keys(references)[i]];
-        if (
-          willCross(
-            xStart,
-            yStart,
-            xEnd,
-            yEnd,
-            secondLine[0],
-            secondLine[2],
-            secondLine[1],
-            secondLine[3]
-          ) &&
-          label != Object.keys(references)[i]
-        ) {
-          console.log(
-            "THIS LINE WILL CROSS " +
-              label +
-              " WITH " +
-              Object.keys(references)[i]
-          );
-        }
-      }
-
-      // Draw line
-      ctx.beginPath();
-      ctx.moveTo(xStart, yStart);
-      ctx.lineTo(xEnd, yEnd);
-      ctx.lineWidth = 4;
-      ctx.shadowBlur = highlighted ? 10 : 0;
-      ctx.lineCap = "round";
-      ctx.shadowColor = "blue";
-      ctx.strokeStyle = highlighted ? "white" : "gray";
-      ctx.stroke();
     });
+
+    generateLines(ctx, 15);
   }, []);
 
   return (
