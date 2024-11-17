cacdb104dada443251ba2bb5d8656e67ba3c642c
Joe Paton <joantpat@gmail.com>
2024-11-17 01:55:08 +0000

    Fixed get leaderboard

--- pages/api/leaderboard/get.js
+++ pages/api/leaderboard/get.js
@@ -4,11 +4,6 @@ import { getUser } from "../user/[uuid]";
 export default async function handler(req, res){
   const client = await clientPromise;
   const db = client.db("AvisCodeLabs");
-  const leaderboard_scores = await db.collection("Leaderboard").find({}).toArray()
-
-  for (let i = 0; i < leaderboard_scores.length; i++){
-    leaderboard_scores[i].username = (await getUser(leaderboard_scores[i].user_id)).username
-  }
-  console.log(leaderboard_scores);
+  const leaderboard_scores = await db.collection("Users").find({}).sort({profit:-1}).limit(10).toArray();
   res.json(leaderboard_scores);
 }
