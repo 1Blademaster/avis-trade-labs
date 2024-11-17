8eb8b1ab583d0a74861c812308d968c3c81cd395
Kush Makkapati <kush.makkapati@icloud.com>
2024-11-17 12:09:41 +0000

    Test another fix

--- engine/engine.js
+++ engine/engine.js
@@ -7,7 +7,7 @@ const valueOffset = -(427500 / 100)
 global.data = []
 const maxDataLength = 4_500_000
 
-const fileNames = ['/combed.csv']
+const fileNames = ['../../public/combed.csv']
 
 function loopThroughFiles(currentFileIndex) {
   console.log(__dirname)
