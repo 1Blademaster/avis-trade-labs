2a1ba2cca26c03f5314e178e111978f517968fd8
Joe Paton <joantpat@gmail.com>
2024-11-17 01:40:25 +0000

    Update leaderboard add

--- pages/api/leaderboard/add.js
+++ pages/api/leaderboard/add.js
@@ -3,5 +3,14 @@ import { ObjectId } from "mongodb";
 export async function addProfit(uuid, profitMade){
   const client = await clientPromise;
   const db = client.db("AvisCodeLabs");
-  db.collection("Leaderboard").updateOne({user_id: new ObjectId(uuid)}, {$inc: {profit: profitMade}})
+  db.collection("Users").updateOne({_id: new ObjectId(uuid)}, {$inc: {profit: profitMade}});
+  db.collection("Leaderboard").insertOne({user_id: new ObjectId(uuid), profit: profitMade});
+}
+
+export default async function handler(req, res){
+  if (req.method !== 'POST') {
+    res.status(405).send({ message: 'Only POST requests allowed' })
+    return
+  }
+  console.log(req.body.user_id);
 }
