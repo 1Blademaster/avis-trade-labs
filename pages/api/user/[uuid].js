7d5e9f71c3e88d3732b4d48f5249533068b653ce
Joe Paton <joantpat@gmail.com>
2024-11-17 01:24:38 +0000

    Added get user api

---
+++ pages/api/user/[uuid].js
@@ -0,0 +1,16 @@
+import { ObjectId } from "mongodb";
+import clientPromise from "../../lib/mongodb"
+
+export async function getUser(uuid){
+    const client = await clientPromise;
+    const db = client.db("AvisCodeLabs");
+    const user = await db.collection("Users").findOne({_id: new ObjectId(uuid)})
+    return user;
+}
+
+export default async function handler(req, res){
+    const { uuid } = req.query;
+    console.log("UUID: " + uuid);
+    const user = await getUser(uuid);
+    res.json(user);
+}
