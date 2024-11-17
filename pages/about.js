23966476af60eaf9051653d40705bb27ddc6e0ee
Julian <julianjones663@gmail.com>
2024-11-17 10:00:29 +0000

    added about us

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,38 @@
 export default function About() {
   return (
-    <div className='container mx-auto'>
-      <h1 className='text-2xl text-center my-8'>About Us</h1>
-      <p className='text-center'>qwfqwf</p>
+    <div className="container mx-auto">
+      <h1 className="pt-8 pb-2 text-2xl text-center">About Us</h1>
+      <p className="text-center">
+        We're a team of drone engineers. Big up avis. Hmu on github{" "}
+        <a
+          href="https://github.com/nexinfinite"
+          target="_blank"
+          class="underline pr-2"
+        >
+          @nexinfinite,
+        </a>
+        <a
+          href="https://github.com/1blademaster"
+          target="_blank"
+          class="underline pr-2"
+        >
+          @1blademaster,
+        </a>
+        <a
+          href="https://github.com/jopat2409"
+          target="_blank"
+          class="underline pr-2"
+        >
+          @jopat2409,
+        </a>
+        <a
+          href="https://github.com/dp1ckford"
+          target="_blank"
+          class="underline"
+        >
+          @dp1ckford
+        </a>
+      </p>
     </div>
-  )
+  );
 }
